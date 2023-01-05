f0e012bb81e20d23edb0a79543d9ca0979980696
Ledagra <svanluven90@gmail.com>
2023-01-05 10:55:10 -0800

    Update maincharacter.js

---
+++ maincharacter.js
@@ -0,0 +1,21 @@
+class MainCharacter {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/pokemonMC.png"), 0, 0, 93, 150, 24, 0.025);
+
+        this.speed = 185;
+        this.x = 0;
+        this.y = 0;
+
+    };
+
+    update() {
+        this.x += this.speed * this.game.clockTick;
+        if(this.x > 1500) this.x = 0;
+
+    };
+
+    draw(ctx) {
+        this.animator.drawframe(this.game.clockTick, ctx, this.x,260)
+    };
+}
